cf24fa032fb0c19be84dc1bd8376c2ea20e3a056
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-01 10:31:58 -0500

    click listeners and post for new todo

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,28 @@ $(onReady);
 
 function onReady() {
 	console.log('We are ready');
+	getList();
+	$(document).on('click', '.checkBox', handleCheckBox);
+	$(document).on('click', '.fa-trash-can', handleDelete);
+	$('.addButton').on('click', createTodo);
+}
+
+function createTodo() {
+	let task = $('.todoInput').val();
+	let newTask = {
+		name: task,
+		complete: false,
+	};
+	$.ajax({
+		type: 'POST',
+		url: '/tasks',
+		data: newTask,
+	})
+		.then(function (response) {
+			console.log(response);
+			getList();
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
 }
