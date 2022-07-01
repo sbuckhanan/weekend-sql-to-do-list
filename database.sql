2c54a9cdada212629d2dec58edd336d733e4b402
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-01 14:27:35 -0500

    update sql file

--- database.sql
+++ database.sql
@@ -8,4 +8,4 @@ CREATE TABLE todos (
     "date-completed" varchar(60)
 );
 
-INSERT INTO todos (name, complete, date-completed) VALUES ('Take out the trash', false, 'July 1, 2022 2:03 PM'), ('Walk the dog', false, 'July 1, 2022 2:03 PM'), ('Do the dishes', false, 'July 1, 2022 2:03 PM');
+INSERT INTO todos (name, complete, date-completed) VALUES ('Take out the trash', false, 'July 1, 2022 12:03 PM'), ('Walk the dog', false, 'July 5, 2022 5:03 PM'), ('Do the dishes', false, 'July 3, 2022 8:03 PM');
